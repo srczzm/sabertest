ead21ce9c6e92596555aaaf87c3c4c81694d7d34
srczzm <74838063+srczzm@users.noreply.github.com>
2021-05-13 09:42:20 +0300

    first push

---
+++ ConsoleApp1/ConsoleApp1/ListClass.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class ListNode
+    {
+        public ListNode Prev { get; set; }
+        public ListNode Next { get; set; }
+        public ListNode Rand { get; set; }
+        public string Data { get; set; }
+
+        public ListNode()
+        {
+            Data = "";
+        }
+        public ListNode(string data)
+        {
+            Data = data;
+        }
+    }
+
+    class ListRand : IEnumerable
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Add(string data)
+        {
+            ListNode node = new ListNode(data);
+
+            if (Head == null)
+                Head = node;
+            else
+            {
+                Tail.Next = node;
+                node.Prev = Tail;
+            }
+            Tail = node;
+            Count++;
+        }
+
+        public void SetRandomNodes()
+        {
+            Random r = new Random();
+            ListNode current = Head;
+
+            while (current != null)
+            {
+                int k = r.Next(0, Count);
+                int j = 0;
+
+                ListNode tmp = Head;
+
+                while (j < k)
+                {
+                    tmp = tmp.Next;
+                    j++;
+                }
+
+                current.Rand = tmp;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            ListNode current = Head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public void Serialize(FileStream s)
+        {
+            List<ListNode> tempList = new List<ListNode>();
+            ListNode tempNode = Head;
+
+            while (tempNode != null)
+            {
+                tempList.Add(tempNode);
+                tempNode = tempNode.Next;
+            }
+
+            if (tempList.Count > 0)
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    foreach (ListNode l in tempList)
+                        sw.WriteLine(l.Data.ToString() + ";" + tempList.IndexOf(l.Rand).ToString());
+
+                    sw.Close();
+                }
+            else
+                Console.WriteLine("Нет узлов для записи");
+        }
+
+        public void Deserialize(FileStream s)
+        {
+            List<ListNode> tempList = new List<ListNode>();
+            List<int> rndList = new List<int>();
+            ListNode tempNode = new ListNode();
+            string line;
+
+            Count = 0;
+            Head = tempNode;
+
+            using (StreamReader sr = new StreamReader(s))
+            {
+                while ((line = sr.ReadLine()) != null && !line.Equals(""))
+                {
+                    ListNode next = new ListNode();
+                    string[] spltLine = line.Split(';');
+
+                    tempNode.Data = spltLine[0];
+                    rndList.Add(Convert.ToInt32(spltLine[1]));
+                    tempNode.Next = next;
+                    Count++;
+                    tempList.Add(tempNode);
+                    next.Prev = tempNode;
+                    tempNode = next;
+                }
+
+                sr.Close();
+            }
+
+            for (int i = 0; i < Count; i++)
+                tempList[i].Rand = tempList[rndList[i]];
+
+            Tail = tempNode.Prev;
+            Tail.Next = null;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListRand linkedList = new ListRand();
+
+            linkedList.Add("item0");
+            linkedList.Add("item1");
+            linkedList.Add("item2");
+            linkedList.Add("item3");
+            linkedList.Add("item4");
+
+            linkedList.SetRandomNodes();
+
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+
+            using (FileStream fs = new FileStream("test.json", FileMode.OpenOrCreate))
+            {
+                linkedList.Serialize(fs);
+                Console.WriteLine("Объект сериализован");
+            }
+
+            linkedList = new ListRand();
+
+            using (FileStream fs = new FileStream("test.json", FileMode.Open))
+            {
+                linkedList.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+            }
+
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
